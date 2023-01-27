bacead09f0ff791c713dceacc78dd2bab49aa53d
leechaeyeon321 <leechaeyeon321@gmail.com>
2023-01-27 22:35:27 +0900

    feat: prac

---
+++ reduxprac/src/store/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import main from './modules/main';
+
+export default combineReducers({
+  main,
+});
---
+++ reduxprac/src/store/modules/main.js
@@ -0,0 +1,14 @@
+const initState = {
+  list: [
+    {
+      id: 1,
+      name: '이채연',
+      comment: '안녕하세요',
+      delete: 'false',
+    },
+  ],
+};
+
+export default function main(state = initState, action) {
+  return state;
+}
