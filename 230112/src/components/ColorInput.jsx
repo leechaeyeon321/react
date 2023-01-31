678a093c53ad0be1d975eabdad87bf8c3da2d2df
leechaeyeon321 <leechaeyeon321@gmail.com>
2023-01-31 19:03:06 +0900

    feat: chaeyeon

--- 230112/src/components/ColorInput.jsx
+++ 230112/src/components/ColorInput.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { useRef } from 'react'
+import React from 'react';
+import { useRef } from 'react';
 
 export default function ColorInput() {
-    const divEl = useRef();
-    const userInput = useRef();
+  const divEl = useRef();
+  const userInput = useRef();
 
-    const adjustCss = () => {
-        divEl.current.style.backgroundColor = userInput.current.value;
-        /* input태그에 있는 값을 넣어주면 됨. */
-    }
+  const adjustCss = () => {
+    divEl.current.style.backgroundColor = userInput.current.value;
+    /* input태그에 있는 값을 넣어주면 됨. */
+  };
   return (
-    <div className='divStyle' ref={divEl}>
-        <input ref={userInput} type="text"/>
-        <br />
-        <br />
-        <button onClick={adjustCss}>색 적용</button>
+    <div className="divStyle" ref={divEl}>
+      <input ref={userInput} type="text" />
+      <br />
+      <br />
+      <button onClick={adjustCss}>색 적용</button>
     </div>
-  )
+  );
 }
