7e5c79f80c098c7cbbe1cce3d00bd750215387e2
leechaeyeon321 <leechaeyeon321@gmail.com>
2023-02-09 19:24:08 +0900

    feat: chaeyeon

--- culture-api/src/components/Culture.jsx
+++ culture-api/src/components/Culture.jsx
@@ -2,30 +2,44 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 export default function Culture() {
-  // const [tempData, setTempData] = useState('');
-  const [culData, setCulData] = useState('');
-  const [loading, setLoading] = useState(true);
+  // // const [tempData, setTempData] = useState('');
+  // const [culData, setCulData] = useState('');
+  // const [loading, setLoading] = useState(true);
 
+  // useEffect(() => {
+  //   const url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=2f86f16e407ccd98d3a2e9f9d7edd11f&targetDt=20120101`;
+  //   axios
+  //     .get(url)
+  //     .then((responseData) => {
+  //       console.log(responseData);
+  //       // const data = responseData.data;
+  //       // setCulData(data.response.body.items.item[0].url);
+  //       setLoading(false);
+  //     })
+  //     .catch((error) => console.log(error));
+  // }, []);
+
+  // if (loading) {
+  //   return <p>Loading</p>;
+  // } else {
+  //   return (
+  //     <>
+  //       {/* <img src={culData} alt="" /> */}
+  //       데이터 확인
+  //     </>
+  //   );
+  // }
   useEffect(() => {
-    const url = `http://api.kcisa.kr/openapi/service/rest/meta2020/getKOCAperf?serviceKey=927183b2-7bfd-4abd-aff7-7ae2ca0940f5&numOfRows=10&pageNo=1`;
     axios
-      .get(url)
-      .then((responseData) => {
-        console.log(responseData);
-        const data = responseData.data;
-        setCulData(data.response.body.items.item[0].url);
-        setLoading(false);
+      .get('https://openapi.naver.com/v1/search/movie.json', {
+        params: { query: '영화이름' },
+        headers: {
+          'X-Naver-Client-Id': 'hdloT8hr8NgBEFkpD04Z',
+          'X-Naver-Client-Secret': 'kG7DAPHBXR',
+        },
       })
-      .catch((error) => console.log(error));
-  }, []);
-
-  if (loading) {
-    return <p>Loading</p>;
-  } else {
-    return (
-      <>
-        <img src={culData} alt="" />
-      </>
-    );
-  }
+      .then((response) => {
+        console.log('response', response.data.items);
+      });
+  });
 }
